6de4203e70c9f138cbdfd08d69da0b1ca47ce2b6
Adrien Joly <adrien@whyd.com>
2017-02-06 13:57:28 +0100

    getting courses, coursework, and student submissions from google classroom

--- tests/test.js
+++ tests/test.js
@@ -1,26 +1,51 @@
 var readline = require('readline');
-var gcal = require('../lib/gcal');
-var mappings = require('../lib/gcal-mappings');
+var gcla = require('../lib/google-classroom');
 
 function whenLogged(err, sesId) {
   if (err) {
     console.error('Error: ', err);
   } else {
-    gcal.listEvents(sesId, function(err, events) {
-      console.log(err || ('displaying ' + events.length + ' events:'));
-      var translatedEvents = events.map(mappings.eventFromGcal);
-      console.log('translated events:', translatedEvents);
-      var eventId = translatedEvents[0].id;
-      console.log('first event id:', eventId);
-      gcal.getEvent(sesId, eventId, function() {
-        console.log('=>', arguments);
+    /*
+    var courseId = '3001621381';
+    var assignmentId = '4287252912';
+    gcla.listSubmissions(courseId, assignmentId, function(err, subs) {
+      console.log(err || subs);
+    });
+    */
+    var courseName = 'JavaScript 1A - Classe 1';
+    var courseWorkTitle = 'TP JS-CSS à rendre: Filtrage de produits';
+
+    var courseByName = (c) => c.name === courseName;
+    var courseWorkByTitle = (c) => c.title === courseWorkTitle;
+    var submissionIsTurnedIn = (s) => s.state === 'TURNED_IN';
+
+    console.log('fetching courses ...');
+    gcla.listCourses(function(err, res) {
+      console.log('=> courses:', err || courses);
+      var courses = (res || {}).courses || [];
+      var courseId = (courses.filter(courseByName)[0] || {}).id;
+      console.log('fetching courseworks for course', courseId, '...');
+      gcla.listCourseWorks(courseId, function(err, res) {
+        console.log('=> courseworks:', err || res);
+        var courseWork = (res || {}).courseWork || [];
+        var courseWorkId = (courseWork.filter(courseWorkByTitle)[0] || {}).id;
+        console.log('fetching submissions for coursework', courseWorkId, '...');
+        gcla.listSubmissions(courseId, courseWorkId, function(err, res) {
+          var subs = (res || {}).studentSubmissions || [];
+          var turnedIn = subs.filter(submissionIsTurnedIn);
+          console.log('=> submissions:', err || JSON.stringify(turnedIn, null, 2));
+          console.log('=> attachments:', turnedIn[0].assignmentSubmission.attachments.map((a) => a.link.url));
+          // =>
+          // - http://jhovine.eemi.tech/Javascript/TP_Filtrage/
+          // - http://codepen.io/SpaCeDuDe/pen/ygEdWr
+        });
       });
     });
   }
 }
 
 function auth(callback) {
-  var authUrl = gcal.generateAuthUrl();
+  var authUrl = gcla.generateAuthUrl();
   console.log('Authorize this app by visiting this url: ', authUrl);
   var rl = readline.createInterface({
     input: process.stdin,
@@ -28,7 +53,7 @@ function auth(callback) {
   });
   rl.question('Enter the code from that page here: ', function(code) {
     rl.close();
-    gcal.getSessionFromCode(code, callback);
+    gcla.getSessionFromCode(code, callback);
   });
 }
 
