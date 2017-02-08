316f16601f0dfce07799694d74d83abd70893945
Adrien Joly <adrien@whyd.com>
2017-02-08 12:17:58 +0100

    refacto: pdf rendering in own file

--- tests/test-pdf.js
+++ tests/test-pdf.js
@@ -1,3 +1,5 @@
+var pdfRenderer = require('../lib/pdf-renderer')
+
 var outputFile = '_test-pdf_output.pdf'
 
 var students = [
@@ -5,45 +7,10 @@ var students = [
   { name: 'b' },
 ]
 
-function renderHtml(students) {
-  function renderStudentHtml(student) {
-    return '<p>' + student.name + '</p>'
-  }
-  return [].concat([
-    '<!DOCTYPE html>',
-    '<html>',
-    '<style>',
-    'body{color:red;}',
-    '</style>',
-    '<body>',
-  ], students.map(renderStudentHtml), [
-    '</body>',
-    '</html>',
-  ]).join('\n')
-}
-
-function genPdfFileFromHtml(filePath, html, callback) {
-  require('phantom').create()
-    .then(function(ph) {
-      return ph.createPage()
-    })
-    .then(function(page) {
-      return page.property('content', html) ? page : null
-    })
-    .then(function(page){
-      return page.render(filePath) ? page : null
-    })
-    .then(function(page) {
-      page.phantom.exit()
-      callback()
-    })
-    .catch(callback)
-}
-
-var html = renderHtml(students)
+var html = pdfRenderer.renderHtml(students)
 
 console.log(html)
 
-genPdfFileFromHtml(outputFile, html, function(err) {
+pdfRenderer.genPdfFileFromHtml(outputFile, html, function(err) {
   console.log(err || 'Page Rendered to ' + outputFile)
 })
