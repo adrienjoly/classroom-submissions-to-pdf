4cd9c690a36c2d21ea2550083fea1a00e52aebee
Adrien Joly <adrien@whyd.com>
2017-02-08 11:06:29 +0100

    refacto: simplified test-apis implementation

--- tests/test-apis.js
+++ tests/test-apis.js
@@ -1,22 +1,10 @@
 var async = require('async')
 var fetchers = require('../lib/fetchers')
 
-async.parallel([
-
-  function testJsFiddle(callback) {
-    fetchers.fetchBySource([ 'jsfiddle', 'u8B29' ], callback)
-  },
-
-  function testJsBin(callback) {
-    fetchers.fetchBySource([ 'jsbin', 'foxego', 6 ], callback)
-  },
-
-  function testCodePen(callback) {
-    fetchers.fetchBySource([ 'codepen', 'adrienjoly', 'WRMWWO' ], callback)
-  },
-
-], function(err, results) {
-  console.log(err || results.map(function(res) {
-    return res.source + ' => ' + JSON.stringify(res, null, 2)//Object.keys(res).join(', ')
-  }).join('\n'))
+async.map([
+  [ 'jsfiddle', 'u8B29' ],
+  [ 'jsbin', 'foxego', 6 ],
+  [ 'codepen', 'adrienjoly', 'WRMWWO' ],
+], fetchers.fetchBySource.bind(fetchers), function(err, results) {
+  console.log(err || results)
 })
