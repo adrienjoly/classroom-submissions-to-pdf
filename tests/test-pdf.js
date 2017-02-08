193122f5134ac68575c845d46cc994ce4a0ef2c7
Adrien Joly <adrien@whyd.com>
2017-02-08 11:49:36 +0100

    refacto: genPdfFileFromHtml()

--- tests/test-pdf.js
+++ tests/test-pdf.js
@@ -1,14 +1,19 @@
-var phantom = require('phantom')
-
 var outputFile = '_test-pdf_output.pdf'
 var html = '<html><style>body{color:red;}</style><body>blah blah blah</body></html>'
 
-phantom.create().then(function(ph) {
-  ph.createPage().then(function(page) {
-    page.property('content', html)
-    page.render(outputFile).then(function() {
-      console.log('Page Rendered to', outputFile)
-      ph.exit()
+function genPdfFileFromHtml(filePath, html, callback) {
+  var phantom = require('phantom')
+  phantom.create().then(function(ph) {
+    ph.createPage().then(function(page) {
+      page.property('content', html)
+      page.render(filePath).then(function() {
+        ph.exit()
+        callback()
+      })
     })
   })
+}
+
+genPdfFileFromHtml(outputFile, html, function(err) {
+  console.log('Page Rendered to', outputFile)
 })
