1f9671b99738ee3855bef4f50bf22f004b49190b
Adrien Joly <adrien@whyd.com>
2017-02-08 11:02:06 +0100

    added fetchers object with one function per source

--- tests/test-apis.js
+++ tests/test-apis.js
@@ -1,44 +1,63 @@
 var async = require('async')
 
-async.parallel([
-
-  function testJsFiddle(callback) {
-    var jsfiddle = require('jsfiddle')
-    var source = [ 'jsfiddle', 'u8B29' ]
-    jsfiddle.getFiddle(source[1], function (err, fiddleObj) {
-      callback(err, Object.assign({ source: source }, fiddleObj))
-    })
+var fetchers = {
+  jsfiddle: function(sourceIds, callback) {
+    require('jsfiddle').getFiddle(sourceIds[0], callback)
   },
-
-  function testJsBin(callback) {
+  jsbin: function(sourceIds, callback) {
     var JsbinClient = require("jsbin-client")
     var jsbin = new JsbinClient({})
-    var source = [ 'jsbin', 'foxego', 6 ]
-    jsbin.read(source[1], source[2]).then(function(data) {
-      callback(null, Object.assign({ source: source }, data))
+    jsbin.read(sourceIds[0], sourceIds[1]).then(function(data) {
+      callback(null, data)
     }, function(error) {
-      callback(error, Object.assign({ source: source }, data))
+      callback(error, data)
     })
   },
-
-  function testCodePen(callback) {
+  codepen: function(sourceIds, callback) {
     var request = require('request')
-    var source = [ 'codepen', 'WRMWWO' ]
-    var penUrl = 'http://codepen.io/adrienjoly/pen/' + source[1]
+    var penUrl = 'http://codepen.io/' + sourceIds[0] + '/pen/' + sourceIds[1]
     async.map(['html', 'js', 'css'], function(ext, callback) {
       request(penUrl + '.' + ext, function (error, response, content) {
         //if (!error && response.statusCode == 200)
-        var obj = { source: source }
+        var obj = {}
         obj[ext] = content
-        callback(error, obj);
+        callback(error, obj)
       })
     }, function(err, results) {
       callback(err, Object.assign.apply(Object, results))
     })
   },
+  fetchBySource: function(source, callback) {
+    var sourceIds = source.slice() // clone array
+    var sourceName = sourceIds.shift()
+    this[sourceName].call(this, sourceIds, function(err, res) {
+      res = res || {}
+      callback(err, {
+        source: source,
+        js: res.js || res.javascript,
+        css: res.css,
+        html: res.html,
+      })
+    })
+  }
+}
+
+async.parallel([
+
+  function testJsFiddle(callback) {
+    fetchers.fetchBySource([ 'jsfiddle', 'u8B29' ], callback)
+  },
+
+  function testJsBin(callback) {
+    fetchers.fetchBySource([ 'jsbin', 'foxego', 6 ], callback)
+  },
+
+  function testCodePen(callback) {
+    fetchers.fetchBySource([ 'codepen', 'adrienjoly', 'WRMWWO' ], callback)
+  },
 
 ], function(err, results) {
   console.log(err || results.map(function(res) {
-    return res.source + ' => ' + Object.keys(res).join(', ')
+    return res.source + ' => ' + JSON.stringify(res, null, 2)//Object.keys(res).join(', ')
   }).join('\n'))
 })
