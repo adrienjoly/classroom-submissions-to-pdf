1e9f3feaf2dfa8ecfa980afefd0d18e8bf96250c
Adrien Joly <adrien@whyd.com>
2017-02-08 11:03:32 +0100

    refacto: fetchers in their own file

--- tests/test-apis.js
+++ tests/test-apis.js
@@ -1,46 +1,5 @@
 var async = require('async')
-
-var fetchers = {
-  jsfiddle: function(sourceIds, callback) {
-    require('jsfiddle').getFiddle(sourceIds[0], callback)
-  },
-  jsbin: function(sourceIds, callback) {
-    var JsbinClient = require("jsbin-client")
-    var jsbin = new JsbinClient({})
-    jsbin.read(sourceIds[0], sourceIds[1]).then(function(data) {
-      callback(null, data)
-    }, function(error) {
-      callback(error, data)
-    })
-  },
-  codepen: function(sourceIds, callback) {
-    var request = require('request')
-    var penUrl = 'http://codepen.io/' + sourceIds[0] + '/pen/' + sourceIds[1]
-    async.map(['html', 'js', 'css'], function(ext, callback) {
-      request(penUrl + '.' + ext, function (error, response, content) {
-        //if (!error && response.statusCode == 200)
-        var obj = {}
-        obj[ext] = content
-        callback(error, obj)
-      })
-    }, function(err, results) {
-      callback(err, Object.assign.apply(Object, results))
-    })
-  },
-  fetchBySource: function(source, callback) {
-    var sourceIds = source.slice() // clone array
-    var sourceName = sourceIds.shift()
-    this[sourceName].call(this, sourceIds, function(err, res) {
-      res = res || {}
-      callback(err, {
-        source: source,
-        js: res.js || res.javascript,
-        css: res.css,
-        html: res.html,
-      })
-    })
-  }
-}
+var fetchers = require('../lib/fetchers')
 
 async.parallel([
 
