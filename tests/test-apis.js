9bb77eff138f5d04b6701c907c0db9d18f359a06
Adrien Joly <adrien@whyd.com>
2017-02-08 10:45:25 +0100

    added codepen parsing + tests

--- tests/test-apis.js
+++ tests/test-apis.js
@@ -1,12 +1,44 @@
-var jsfiddle = require('jsfiddle')
-jsfiddle.getFiddle('u8B29', function (err, fiddleObj) {
-  console.log('jsfiddle =>', err || fiddleObj)
-})
+var async = require('async')
 
-var JsbinClient = require("jsbin-client")
-var jsbin = new JsbinClient({})
-jsbin.read('foxego', 6).then(function(data) {
-  console.log('jsbin =>', data)
-}, function(error) {
-  console.error('jsbin =>', error)
+async.parallel([
+
+  function testJsFiddle(callback) {
+    var jsfiddle = require('jsfiddle')
+    var source = [ 'jsfiddle', 'u8B29' ]
+    jsfiddle.getFiddle(source[1], function (err, fiddleObj) {
+      callback(err, Object.assign({ source: source }, fiddleObj))
+    })
+  },
+
+  function testJsBin(callback) {
+    var JsbinClient = require("jsbin-client")
+    var jsbin = new JsbinClient({})
+    var source = [ 'jsbin', 'foxego', 6 ]
+    jsbin.read(source[1], source[2]).then(function(data) {
+      callback(null, Object.assign({ source: source }, data))
+    }, function(error) {
+      callback(error, Object.assign({ source: source }, data))
+    })
+  },
+
+  function testCodePen(callback) {
+    var request = require('request')
+    var source = [ 'codepen', 'WRMWWO' ]
+    var penUrl = 'http://codepen.io/adrienjoly/pen/' + source[1]
+    async.map(['html', 'js', 'css'], function(ext, callback) {
+      request(penUrl + '.' + ext, function (error, response, content) {
+        //if (!error && response.statusCode == 200)
+        var obj = { source: source }
+        obj[ext] = content
+        callback(error, obj);
+      })
+    }, function(err, results) {
+      callback(err, Object.assign.apply(Object, results))
+    })
+  },
+
+], function(err, results) {
+  console.log(err || results.map(function(res) {
+    return res.source + ' => ' + Object.keys(res).join(', ')
+  }).join('\n'))
 })
