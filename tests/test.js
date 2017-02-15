334ddd35489a5b1c65f73bf2cac562060f49d5b5
Adrien Joly <adrien@whyd.com>
2017-02-15 16:38:14 +0100

    toHTML() helper

--- tests/test.js
+++ tests/test.js
@@ -6,23 +6,15 @@ converter.extractSubmissions(sampleSubmissions, function(err, studentSubmissions
   if (err) {
     console.log('=>', err)
   } else {
+    /*
     studentSubmissions.forEach(function(subm, i) {
       var id = subm._gcla_subm.userId
       console.log('- submission #' + id, '=>', subm.fetchedAttachments.map(function(att) {
         return att.err || (att.source.join(':') + ' -> ' + Object.keys(att).join(', '))
       }))
     })
-    /*
-    var html = converter.toHtml(studentSubmissions);
+    */
+    var html = converter.toHTML(studentSubmissions);
     console.log('\n=> HTML:', html)
-    */
-
-    /*
-    var pdfRenderer = require('../lib/pdf-renderer')
-    var html = pdfRenderer.renderHtml(studentSubmissions)
-    pdfRenderer.genPdfFileFromHtml(outputFile, html, function(err) {
-      console.log(err || 'Page Rendered to ' + outputFile)
-    })
-    */
   }
 })
