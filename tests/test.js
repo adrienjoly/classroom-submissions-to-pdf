abc21674fe92a578045a4b0eb9ee5dd96c1456fb
Adrien Joly <adrien@whyd.com>
2017-02-15 16:23:19 +0100

    api: extractSubmission => [ { fetchedAttachment, _gcla_subm } ]

--- tests/test.js
+++ tests/test.js
@@ -7,16 +7,22 @@ converter.extractSubmissions(sampleSubmissions, function(err, studentSubmissions
     console.log('=>', err)
   } else {
     studentSubmissions.forEach(function(subm, i) {
-      console.log('- submission #' + i, '=>', subm.map(function(subm) {
-        return subm.err || (subm.source.join(':') + ' -> ' + Object.keys(subm).join(', '))
+      var id = subm._gcla_subm.userId
+      console.log('- submission #' + id, '=>', subm.fetchedAttachments.map(function(att) {
+        return att.err || (att.source.join(':') + ' -> ' + Object.keys(att).join(', '))
       }))
     })
+    /*
+    var html = converter.toHtml(studentSubmissions);
+    console.log('\n=> HTML:', html)
+    */
+
+    /*
+    var pdfRenderer = require('../lib/pdf-renderer')
+    var html = pdfRenderer.renderHtml(studentSubmissions)
+    pdfRenderer.genPdfFileFromHtml(outputFile, html, function(err) {
+      console.log(err || 'Page Rendered to ' + outputFile)
+    })
+    */
   }
-  /*
-  var html = pdfRenderer.renderHtml(studentSubmissions)
-  console.log(html)
-  pdfRenderer.genPdfFileFromHtml(outputFile, html, function(err) {
-    console.log(err || 'Page Rendered to ' + outputFile)
-  })
-  */
 })
