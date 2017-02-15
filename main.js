bfdc8630c7ad4438b78c3253fe6eedf579594e63
Adrien Joly <adrien@whyd.com>
2017-02-15 15:29:21 +0100

    refacto: submission-converter.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var readline = require('readline');
-var async = require('async')
 var gcla = require('./lib/google-classroom')
-var fetchers = require('./lib/fetchers')
 var pdfRenderer = require('./lib/pdf-renderer')
 
 // inputs (classroom assignment)
@@ -11,38 +9,18 @@ var assignmentId = '4287252912';
 // output (PDF-rendered student submissions)
 var outputFile = 'student-submissions.pdf'
 
+var extractSubmissions = require('./lib/submission-converter').extractSubmissions
+
+/*
 var submissionIsTurnedIn = (s) => s.state === 'TURNED_IN';
 
-function processSubmissions(turnedIn) {
-  async.mapSeries(turnedIn, function(submission, callback) {
-    console.log('submission.assignmentSubmission', submission.assignmentSubmission)
-    var urls = submission.assignmentSubmission.attachments.map((a) => a.link.url)
-    async.mapSeries(urls, function(url, callback) {
-      console.log('\n' + url + '...')
-      fetchers.fetchByUrl(url, callback)
-    }, function(err, results) {
-      console.log('results:', err || results)
-    })
-  }, function(err, results) {
-    console.log('-', err || results)
-  }, function(err, studentSubmissions) {
-    var html = pdfRenderer.renderHtml(studentSubmissions)
-    console.log(html)
-    /*
-    pdfRenderer.genPdfFileFromHtml(outputFile, html, function(err) {
-      console.log(err || 'Page Rendered to ' + outputFile)
-    })
-    */
-  })
-}
-/*
 function whenLogged(err, sesId) {
   if (err) {
     console.error('Error: ', err);
   } else {
     gcla.listSubmissions(courseId, assignmentId, function(err, subs) {
       console.log('submissions from classroom =>', err || subs.studentSubmissions.length)
-      processSubmissions(subs.studentSubmissions.filter(submissionIsTurnedIn))
+      extractSubmissions(subs.studentSubmissions.filter(submissionIsTurnedIn))
     })
   }
 }
@@ -63,4 +41,4 @@ function auth(callback) {
 auth(whenLogged);
 */
 
-processSubmissions([ require('./samples/turned-in-student-submission.json') ])
+extractSubmissions([ require('./samples/turned-in-student-submission.json') ])
