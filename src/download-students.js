ace92bfb6010199411c5573a1ca95a7dd1d9438f
Adrien Joly <adrien@whyd.com>
2017-02-17 11:29:27 +0100

    download students

---
+++ src/download-students.js
@@ -0,0 +1,29 @@
+var async = require('async')
+var gcla = require('../lib/google-classroom')
+var googleClient = require('../lib/google-api-client')
+
+const groups = {
+  1: { courseId: 3001621381 },
+  2: { courseId: 3001638443 },
+  3: { courseId: 3001624136 },
+}
+
+const saveJSON = (file, o) => require('fs').writeFileSync(file, JSON.stringify(o, null, 2))
+
+function fetchStudents(classe, callback) {
+  console.log('fetching students of classe', classe, '...')
+  gcla.listStudents(groups[classe].courseId, function(err, res) {
+    console.log('=> students:', err || res.students.length)
+    saveJSON('students-classe-' + classe + '.json', res)
+    callback(err, res)
+  })
+}
+
+// auth using user_token.json or oauth
+googleClient.auth(function whenLogged(err, sesId) {
+  if (err) {
+    console.error('Error: ', err)
+  } else {
+    async.mapSeries(Object.keys(groups), fetchStudents, () => console.log('done.'))
+  }
+})
