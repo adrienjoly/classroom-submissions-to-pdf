f1aa52f7b55214f3a7ef8bb60f7250b29e665641
Adrien Joly <adrien@whyd.com>
2017-02-17 11:41:47 +0100

    moved convert-submissions script to src/

--- src/convert-submissions.js
+++ src/convert-submissions.js
@@ -1,11 +1,12 @@
 var fs = require('fs')
-var converter = require('./lib/submission-converter')
+var converter = require('../lib/submission-converter')
 
 // output (PDF-rendered student submissions)
-var outputFile = './submissions-turned-in.pdf'
+var outputFile = './submissions-classe-1.pdf' // TODO: for each group
+var htmlOutputFile = './submissions-classe-1.html' // TODO: for each group
 
 // input
-const inputFile = './submissions-turned-in.json'
+const inputFile = '../submissions-classe-1.json' // TODO: for each group
 
 const turnedIn = require(inputFile)
 
@@ -13,14 +14,12 @@ converter.extractSubmissions(turnedIn, function(err, studentSubmissions) {
   if (err) {
     console.log('=>', err)
   } else {
-    var html = converter.toHTML(studentSubmissions);
-    fs.writeFileSync('./submissions-turned-in.html', html);
+    var html = converter.toHTML(studentSubmissions)
+    fs.writeFileSync(htmlOutputFile, html)
     converter.toPDF(outputFile, studentSubmissions, function(err) {
       console.log(err || 'Page Rendered to ' + outputFile)
     })
   }
 })
-
-// TODO: also support links in comments
 
 // TODO: also support generic links (from any source)
