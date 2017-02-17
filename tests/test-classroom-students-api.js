81906eb96eb3a235d0c2d4ae9f58a7c18244f593
Adrien Joly <adrien@whyd.com>
2017-02-17 11:17:27 +0100

    list students

--- tests/test-classroom-students-api.js
+++ tests/test-classroom-students-api.js
@@ -1,42 +1,20 @@
-var readline = require('readline')
 var gcla = require('../lib/google-classroom')
 var googleClient = require('../lib/google-api-client')
 
-var courseName = 'JavaScript 1A - Classe 2'
-var courseWorkTitle = 'TP JS-CSS à rendre: Filtrage de produits'
+var [ courseId, assignmentId ] = [ '3001638443', '4287252910' ] // classe 2
 
-var courseByName = (c) => c.name === courseName
-var courseWorkByTitle = (c) => c.title === courseWorkTitle
-var submissionIsTurnedIn = (s) => s.state === 'TURNED_IN'
-
-function whenLogged(err, sesId) {
-  if (err) {
-    console.error('Error: ', err)
-  } else {
-    console.log('fetching courses ...')
-    gcla.listCourses(function(err, res) {
-      console.log('=> courses:', err || courses)
-      var courses = (res || {}).courses || []
-      var courseId = (courses.filter(courseByName)[0] || {}).id
-      console.log('fetching courseworks for course', courseId, '...')
-      gcla.listCourseWorks(courseId, function(err, res) {
-        console.log('=> courseworks:', err || res)
-        var courseWork = (res || {}).courseWork || []
-        var courseWorkId = (courseWork.filter(courseWorkByTitle)[0] || {}).id
-        console.log('fetching submissions for coursework', courseWorkId, '...')
-        gcla.listSubmissions(courseId, courseWorkId, function(err, res) {
-          var subs = (res || {}).studentSubmissions || []
-          var turnedIn = subs.filter(submissionIsTurnedIn)
-          console.log('=> submissions:', err || JSON.stringify(turnedIn, null, 2))
-          console.log('=> attachments:', turnedIn[0].assignmentSubmission.attachments.map((a) => a.link.url))
-          // =>
-          // - http://jhovine.eemi.tech/Javascript/TP_Filtrage/
-          // - http://codepen.io/SpaCeDuDe/pen/ygEdWr
-        })
-      })
-    })
-  }
+function fetchStudents() {
+  console.log('fetching students ...')
+  gcla.listStudents(courseId, function(err, students) {
+    console.log('=> students:', err || JSON.stringify(students, null, 2))
+  })
 }
 
 // auth using user_token.json or oauth
-googleClient.auth(whenLogged)
+googleClient.auth(function whenLogged(err, sesId) {
+  if (err) {
+    console.error('Error: ', err)
+  } else {
+    fetchStudents()
+  }
+})
