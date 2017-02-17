ea26a185f8ccabeb64554205f363bcb70798fef5
Adrien Joly <adrien@whyd.com>
2017-02-17 11:02:46 +0100

    test.js: display results progressively

--- tests/test.js
+++ tests/test.js
@@ -1,26 +1,28 @@
+var async = require('async')
 var converter = require('../lib/submission-converter')
 
 const outputFile = '_test-pdf_output.pdf'
 
-const sampleSubmissions = [ require('../samples/turned-in-student-submission.json') ]
+//const sampleSubmissions = [ require('../samples/turned-in-student-submission.json') ]
+const sampleSubmissions = require('../submissions-classe-3.json')
 
-converter.extractSubmissions(sampleSubmissions, function(err, studentSubmissions) {
-  if (err) {
-    console.log('=>', err)
-  } else {
-    /*
-    studentSubmissions.forEach(function(subm, i) {
-      var id = subm._gcla_subm.userId
-      console.log('- submission #' + id, '=>', subm.fetchedAttachments.map(function(att) {
+async.mapSeries(sampleSubmissions, function(subm, callback) {
+  console.log('\nsubmission', subm.userId)
+  converter.extractSubmission(subm, function(err, subm) {
+    if (err) {
+      console.log('=>', err)
+      callback(err)
+    } else {
+      console.log('=>', subm.fetchedAttachments.map(function(att) {
         return att.err || (att.source.join(':') + ' -> ' + Object.keys(att).join(', '))
       }))
-    })
-    */
-    var html = converter.toHTML(studentSubmissions);
-    //console.log('\n=> HTML:', html)
-
-    converter.toPDF(outputFile, studentSubmissions, function(err) {
-      console.log(err || 'Page Rendered to ' + outputFile)
-    })
-  }
+      callback(null, subm)
+    }
+  })
+}, function(err, studentSubmissions) {
+  //var html = converter.toHTML(studentSubmissions)
+  //console.log('\n=> HTML:', html)
+  converter.toPDF(outputFile, studentSubmissions, function(err) {
+    console.log(err || 'Page Rendered to ' + outputFile)
+  })
 })
