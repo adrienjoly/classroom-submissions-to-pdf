a73228103b7744ba81117ab6a39a8909c979a37f
Adrien Joly <adrien@whyd.com>
2017-02-20 10:28:41 +0100

    convert-submissions generates JSON files instead of rendering immediately to PDF

--- src/convert-submissions.js
+++ src/convert-submissions.js
@@ -5,23 +5,16 @@ var converter = require('../lib/submission-converter')
 // inputs
 const groups = require('../groups.json')
 
+const saveJSON = (file, o) => require('fs').writeFileSync(file, JSON.stringify(o, null, 2))
+
 function convertSubmissions(classe, callback) {
   console.log('\nconverting submissions of classe', classe, '...')
   const filename = 'submissions-classe-' + classe
   const submissions = require('../' + filename + '.json')
   converter.extractSubmissions(submissions, function(err, studentSubmissions) {
-    if (err) {
-      console.log('=>', err)
-      callback(err)
-    } else {
-      var html = converter.toHTML(studentSubmissions)
-      fs.writeFileSync(filename + '.html', html)
-      console.log('\ngenerating PDF file...')
-      converter.toPDF(filename + '.pdf', studentSubmissions, function(err, res) {
-        console.log(err || '=> rendered to ' + filename + '.pdf')
-        callback(err, res)
-      })
-    }
+    saveJSON(filename + '-converted.json', studentSubmissions)
+    console.log(err || '=> rendered to ' + filename + '-converted.json')
+    callback(err, studentSubmissions)
   })
 }
 
